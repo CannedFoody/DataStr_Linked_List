d75f2a4c472dcc4dc0c0aae4abc8d625be5d9c3b
CannedFoody <gamingman72069@gmail.com>
2026-03-20 18:36:41 +0200

    Complete MyNode class.

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,44 @@
 package datastr;
 
 public class MyNode {
+	private char element;
+	private MyNode previous_node = null;
+	private MyNode next_node = null;
+	
+//	Getters
+	public char getElement() {
+		return element;
+	}
 
+	public MyNode getPrevious_node() {
+		return previous_node;
+	}
+	
+	public MyNode getNext_node() {
+		return next_node;
+	}
+	
+//	Setters
+	public void setElement(char element) {
+		this.element = element;
+	}
+	
+	public void setPrevious_node(MyNode previous_node) {
+		this.previous_node = previous_node;
+	}
+
+	public void setNext_node(MyNode next_node) {
+		this.next_node = next_node;
+	}
+	
+//	Constructors
+	
+	public MyNode(char element) {
+		setElement(element);
+	}
+	
+	public String print() {
+		return "" + this.element;
+	}
+	
 }

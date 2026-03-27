40c69fb81d6e35d3eefed1c812edcfd12ac9233a
CannedFoody <gamingman72069@gmail.com>
2026-03-27 16:55:19 +0200

    Added a function that adds an element at a certain position.

--- src/datastr/MyLinkedList.java
+++ src/datastr/MyLinkedList.java
@@ -66,4 +66,81 @@ public class MyLinkedList {
 		
 		return element_count;
 	}
+	
+	public void add(char element, int index) throws Exception {
+		if (this.is_full()) {
+			throw new Exception("Cannot add new element, the LinkedList is full...");
+		}
+		
+		if (index > this.size || index < 0) {
+			throw new Exception("Cannot add new element at that index, index out of bounds...");
+		}
+		
+		MyNode new_node = new MyNode(element);
+		
+		if(index == 0) {
+			first_node.set_previous_node(new_node);
+			new_node.set_next_node(first_node);
+			this.first_node = new_node;
+			this.size += 1;
+		}
+		
+		if(index == this.size) {
+			this.add(element);
+		}
+		
+		else {
+			if(index < (size / 2)) {
+				MyNode current_node = this.first_node;
+				int pass_counter = 0;
+				
+				MyNode before_node = null;
+				MyNode after_node = null;
+				
+				while(!current_node.get_next_node().equals(null)) {
+					if (pass_counter == index) {
+						before_node = current_node;
+						after_node = current_node.get_next_node();
+						break;
+					}
+					current_node = current_node.get_next_node();
+					pass_counter += 1;
+				}
+				
+				before_node.set_next_node(new_node);
+				after_node.set_previous_node(new_node);
+				
+				new_node.set_next_node(after_node);
+				new_node.set_previous_node(before_node);
+				
+				this.size += 1;
+			}
+			else {
+				MyNode current_node = this.last_node;
+				int pass_counter = this.size;
+				
+				MyNode before_node = null;
+				MyNode after_node = null;
+				
+				while(!current_node.get_previous_node().equals(null)) {
+					if(pass_counter == index) {
+						after_node = current_node;
+						before_node = current_node.get_previous_node();
+						break;
+					}
+					
+					current_node = current_node.get_previous_node();
+					pass_counter -= 1;
+				}
+				
+				before_node.set_next_node(new_node);
+				after_node.set_previous_node(new_node);
+				
+				new_node.set_next_node(after_node);
+				new_node.set_previous_node(before_node);
+				
+				this.size += 1;
+			}
+		}
+	}
 }
